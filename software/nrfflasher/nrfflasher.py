b36b1c6c9d9fc30cfd2a3e9740477ad095e27b3b
Chereq <mr.dead.snyper@gmail.com>
2014-04-13 18:38:51 +0400

    1'st version of PC-side program (find device, get f/w version)

--- software/nrfflasher/nrfflasher.py
+++ software/nrfflasher/nrfflasher.py
@@ -1,28 +1,39 @@
-#! /usr/bin/env python
-from re import search
-
+﻿#! /usr/bin/env python
 import serial
 from time import sleep
 
-print "Hello nrf24LE1! :)\n"
+def device_search():
+	for port_num in range(256):
+		answer = None
+		try:
+			serial_port = serial.Serial(port_num, 9600, timeout=1)
+			sleep(1.6)
+			serial_port.write('T')
+			answer = serial_port.read(2)
+			port_name = serial_port.portstr
+			serial_port.close()
+		except serial.SerialException:
+			pass
+		if answer == 'OK':
+			return port_num, port_name
+	return None
 
+def main():
+	print 'Hello nrf24LE1! :)\n'
+	device_port = device_search()
+	if not device_port:
+		print '404 Device Not Found'
+		exit(1)
+	print 'Device port:', device_port
+	serial_port = serial.Serial(device_port[0], 9600, timeout=1)
+	sleep(1.6)
+	serial_port.write('V')
+	answer = serial_port.read(15)
+	serial_port.close()
+	if answer[-2:] <> 'OK':
+		print '502 Bad Device'
+		exit(2)
+	print 'Device f/w version:', answer[:-2]
 
-serialPort = serial.Serial('COM10', 9600, timeout=1)
-sleep(2)
-serialPort.write('T')
-print serialPort.read(100)
-sleep(.1)
-
-serialPort.write('T')
-print serialPort.read(100)
-sleep(.1)
-
-serialPort.write('T')
-print serialPort.read(100)
-sleep(.1)
-
-serialPort.write('T')
-print serialPort.read(100)
-sleep(.1)
-
-
+if __name__ == '__main__':
+	main()
