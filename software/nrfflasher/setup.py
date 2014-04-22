42eca853ceb8152edd90670ca0ba4314e286a0ef
Chereq <mr.dead.snyper@gmail.com>
2014-04-22 04:32:34 -0700

    Update py2exe compiler script

--- software/nrfflasher/setup.py
+++ software/nrfflasher/setup.py
@@ -2,7 +2,12 @@ from distutils.core import setup
 import py2exe
   
 setup(
-    console=[{"script":"nrfflasher.py"}], 
-    options={"py2exe": {"includes":["serial", "os", "sys", "time"]}},
-    zipfile=None
+	console=[{"script":"nrfflasher.py"}], 
+	options={"py2exe": {
+		"includes":["serial", "os", "sys", "time"],
+		"dll_excludes":["w9xpopen.exe"],
+		"compressed": 1, 
+		"optimize": 2, 
+		"bundle_files": 1}},
+	zipfile=None
 )
